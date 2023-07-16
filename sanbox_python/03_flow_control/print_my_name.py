1df8ee38c7b1eaec17a6af290b1537d326728b0f
Emmanuel Tar <emmanueltar14@gmail.com>
2023-07-16 15:32:26 +0100

    Iterating Character in my Name

---
+++ sanbox_python/03_flow_control/print_my_name.py
@@ -0,0 +1,16 @@
+"""
+    Now I am in Zortan Planet, I want to do Things Differently
+    Zortan People spell their name with each character on different line
+"""
+
+#----------------------
+# Variable declaration
+#----------------------
+Name:str = 'Emmanuel'
+
+# create a 'for loop to iterate the character in Emmanuel
+
+for character in Name:
+    print(character)
+
+
